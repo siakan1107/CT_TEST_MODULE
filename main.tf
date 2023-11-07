0b1d47dbee61385063314ef4d29ad7a21a97c855
kibeom <siakan1107@gmail.com>
2023-11-07 22:30:18 +0900

    TEST

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ module "identity_center_dpc" {
   vcs_repo_identifier     = var.vcs_repo_identifier
   vcs_repo_oauth_token_id = var.vcs_repo_oauth_token_id
 
-  count                       = var.managed_policies != null ? 1 : 0
+  count                       = var.managed_policies == null ? 1 : 0
   managed_policies            = var.managed_policies
   custom_policies             = var.custom_policies
   tfc_provider_arn            = local.tfc_provider_arn
