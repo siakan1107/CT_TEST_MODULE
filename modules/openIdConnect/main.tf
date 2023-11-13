9ce5037cc7215451c886fa6aba08d1f87a286275
kibeom <siakan1107@gmail.com>
2023-11-13 22:02:56 +0900

    test

--- modules/openIdConnect/main.tf
+++ modules/openIdConnect/main.tf
@@ -5,3 +5,13 @@ output "tfc_provider_arn" {
 output "tfc_provider_client_id_list" {
   value = aws_iam_openid_connect_provider.tfc_provider.client_id_list
 }
+
+data "tls_certificate" "tfc_certificate" {
+  url = "https://${var.tfc_hostname}"
+}
+
+resource "aws_iam_openid_connect_provider" "tfc_provider" {
+  url             = data.tls_certificate.tfc_certificate.url
+  client_id_list  = [var.tfc_aws_audience]
+  thumbprint_list = [data.tls_certificate.tfc_certificate.certificates[0].sha1_fingerprint]
+}
