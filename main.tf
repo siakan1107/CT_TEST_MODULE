983d0a5467023e23195943d5351bd191e046f5d9
kibeom <siakan1107@gmail.com>
2023-11-13 23:13:43 +0900

    test

--- main.tf
+++ main.tf
@@ -1,22 +1,18 @@
 locals {
   oauth_token_id = "ot-gZ8WnNTbRFDhkJtn"
-  # tfc_provider_arn            = aws_iam_openid_connect_provider.tfc_provider.arn
-  # tfc_provider_client_id_list = aws_iam_openid_connect_provider.tfc_provider.client_id_list
 }
 
-module "identity_center_dpc" {
-  source                = "./modules/"
-  tfc_hostname          = var.tfc_hostname
-  tfc_organization_name = var.tfc_organization_name
-  tfc_project_name      = var.tfc_project_name
-
+module "main_dpc" {
+  source                      = "./modules/"
+  tfc_hostname                = var.tfc_hostname
+  tfc_organization_name       = var.tfc_organization_name
+  tfc_project_name            = var.tfc_project_name
   tfc_provider_arn            = var.tfc_provider_arn
   tfc_provider_client_id_list = var.tfc_provider_client_id_list
-
-  dpc_name                = var.dpc_name
-  vcs_repo_identifier     = var.vcs_repo_identifier
-  vcs_repo_oauth_token_id = local.oauth_token_id
-  managed_policies        = var.managed_policies
-  custom_policies         = var.custom_policies
+  dpc_name                    = var.dpc_name
+  vcs_repo_identifier         = var.vcs_repo_identifier
+  vcs_repo_oauth_token_id     = local.oauth_token_id
+  managed_policies            = var.managed_policies
+  custom_policies             = var.custom_policies
 
 }
